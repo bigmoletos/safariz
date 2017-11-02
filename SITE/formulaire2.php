0e31b16789b4e4a2e746aa57d4acf38dd7946124
franck <bigmoletos@yopmail.com>
2017-11-02 13:34:13 +0100

    mise en forme formulaire, chekbox, message, reglement

--- SITE/formulaire2.php
+++ SITE/formulaire2.php
@@ -1,4 +1,8 @@
-<?php ?>
+<?php 
+
+
+
+?>
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -16,7 +20,7 @@ and open the template in the editor.
         <!--<script src="style/jqueryFiles/jquery-3.2.1.min.js"></script>-->
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
         <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
-          <script type="text/javascript" src="style/JQueryFiles/jquery.maskedinput-master/dist/jquery.maskedinput.min.js"></script>
+        <script type="text/javascript" src="style/JQueryFiles/jquery.maskedinput-master/dist/jquery.maskedinput.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
         <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" /> 
         <link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css" />
@@ -24,11 +28,12 @@ and open the template in the editor.
       
     </head>
 
-    <body class="container">
-        <div class="bootstrap-iso">
+    <body class="container" >
+        <div class="well">
+          <div class="bootstrap-iso">
             <div class="container-fluid">
                 
-                 <div class="row"><!--titre-->
+                 <div class="row" ><!--titre-->
                     <div class="col-md-6 col-sm-6 col-xs-12">
                         <h1>Inscrivez-vous ci-dessous:</h1>
                         <p>Tous les champs marqués d'une <span class="asteriskField">*</span> sont obligatoire</p>
@@ -39,16 +44,16 @@ and open the template in the editor.
                 
                 <div class="row"><!--nom et prenom-->
                     <div class="col-md-3 col-sm-3 col-xs-12">
-                        <form method="post" name="inscription" action="index.php">
+                        <form method="post" name="inscription" action="formulaire2.php">
                             <div class="form-group ">
-                                <label class="control-label requiredField" for="nom">NOM<span class="asteriskField">*</span></label>
+                                <label class="control-label requiredField" for="nom">Nom<span class="asteriskField">*</span></label>
                                 <input class="form-control" id="nom" name="nom"   type="text" placeholder="votre nom...."  maxlength="48"/>
                             </div>
 
                     </div>
                     <div class="col-md-3 col-sm-3 col-xs-12 offset-1">
                             <div class="form-group ">
-                                <label class="control-label requiredField" for="prenom">PRENOM <span class="asteriskField"> * </span></label>
+                                <label class="control-label requiredField" for="prenom">Prénom<span class="asteriskField"> * </span></label>
                                 <input class="form-control" id="prenom" name="prenom"  type="text" placeholder="votre prénom...." maxlength="48"/>
                              </div> 
                     </div>
@@ -57,7 +62,7 @@ and open the template in the editor.
          <div class="row"><!--adresse-->
                     <div class="col-md-6 col-sm-6 col-xs-12">
                             <div class="form-group ">
-                                <label class="control-label requiredField" for="adresse">adresse<span class="asteriskField">*</span></label>
+                                <label class="control-label requiredField" for="adresse">Adresse<span class="asteriskField">*</span></label>
                                 <div class="input-group">
                                     <div class="input-group-addon"><i class="fa fa-bus"></i>
                                     </div>
@@ -128,14 +133,13 @@ and open the template in the editor.
                     </div>
                 </div>
 
-            <br/>
+                <div id="espace"> </div><!--espace entre bouton et reglement-->
 
-                <div class="row"><!--majeur-->
+                <div class="row"><!--majeur et bouton validation-->
                    <div class="col-md-3 col-sm-3 col-xs-12"> 
-                                  <input type="checkbox" name="majeur" value="ON" /> je reconnais être majeur<span class="asteriskField">*</span>
+                                  <input type="checkbox" id="majeur" name="majeur" value="ON" onClick=""/> je reconnais être majeur<span class="asteriskField">*</span>
                    </div>
 <!--                </div>-->
-
 <!--
                <br/><br/>
                 <div class="row">bouton validation-->
@@ -148,13 +152,12 @@ and open the template in the editor.
                        </div>
                    </div>
                </div>
-                            <!--</div>-->
-                        </form>
-
-                    </div>
-                </div>
-            </div>
-        </div>
+        </form>    
+          </div>
+       </div>
+    </div>
+  </div>
+</div>
                 <script type="text/javascript">
                    $(document).ready( $(function () {
                         //  $("#date").mask("99/99/9999");
@@ -169,8 +172,38 @@ and open the template in the editor.
 //                                                });
 //                                                
                         $("#email").mask('Regex', { regex: "[a-zA-Z0-9._%-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,4}" });
-                    }));//fin function
+                  
+    //fonction de controle de saisie du formulaire
+    
+   function testerCheckbox(checkbox) {
+      for (var i=0; i<checkbox.length;i++) {
+         if (checkbox[i].checked) {
+            alert("Système = "+checkbox[i].value)
+         }
+      }
+   }
+
+    
+    //fonction verification mail
+    function verifiermail(email) {
+      if ((email_add.indexOf("@")!=1) && (email_add.indexOf(".")!=2)) {
+         return true 
+      } else {
+         alert("Mail invalide !");
+         return false
+      };
+      
+   };
+    
+    
+    }));//fin function
                    //fin document ready
+                
+                
+                
+                
+                
+                
                 </script>
 
     </body>
