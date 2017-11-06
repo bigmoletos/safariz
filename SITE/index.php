39c9cdf9c81c95be6241453ab625034fdeb70cfe
JC Martinez <jan.chrim@gmail.com>
2017-11-06 17:15:58 +0100

    design MAJ

---
+++ SITE/index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<title>Safa'riz Le Jeu</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="style/styleFormulaire.css" type="text/css" charset="utf_8"/>
+	<link rel="stylesheet" href="font/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link href="style/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="style/style.css">
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+	<script type="text/javascript" src="style/JQueryFiles/jquery.maskedinput-master/dist/jquery.maskedinput.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link rel="stylesheet" href="style/bootstrap-iso.css"/>
+	<link rel="stylesheet" href="https://formden.com/static/cdn/font-awesome/4.4.0/css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<body class="container">
+	<section id="header" class="row">
+		<div id="logo_safariz" class="col-sm-12 col-md-3">
+                    <a href="index.php"><img src="image/safariz.png" alt="logo safariz" height="190" width="250" responsive-image></a>
+		</div>
+
+		<main id="content" class="col-sm-12 col-md-9">
+
+
+			<!-- integration formulaire de Franck -->
+			<div class="bootstrap-iso">
+				<!-- <div class="container-fluid"> -->
+
+				<div class="well">
+					<div class="row">
+						<!--titre-->
+						<div class="col-12">
+							<h1>Cliquez sur le bouton "jouer" pour participer à notre jeu Safa'Riz :</h1>
+						</div>
+					</div>
+
+
+					
+
+					<div id="espace"> </div>
+					<!--espace entre bouton et reglement-->
+
+					<div class="row">
+						
+						<!--                </div>-->
+						<!--
+               <br/><br/>
+                <div class="row">bouton validation-->
+
+						<div class="col-md-6 col-sm-6 col-xs-12">
+							<div class="form-group">
+								<div>
+									<a href="form.php"><button class="btn btn-primary " name="bouton" type="submit">Jouer maintenant</button>
+                                                                        <!-- bouton jouer sur page indesx -->
+								</div>
+							</div>
+						</div>
+					</div>
+					</form>
+				</div>
+				<!--</div> container.fluid-->
+			</div>
+			</div>
+			</div>
+			
+
+
+		</main>
+		<!-- content -->
+                </section>
+                
+<?php include("footer.php"); ?>
+                      
+	
+
+			
+
+</body>
+</html>
