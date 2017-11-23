ce3de91244bfbdd52cd282062646b0fac1ffb473
franck <bigmoletos@yopmail.com>
2017-11-23 20:40:01 +0100

    modif suite push jc

---
+++ SITE/footer_1.php
@@ -0,0 +1,33 @@
+
+<div>
+            <div id="logos" class="col-md-3">
+            <ul class="list-inline" >
+                <li class="col-md-6"><img src="image/logo_riz_camargues.png" alt="riz de camargues" height="121" width="112" responsive-image></li>
+                <li class="col-md-6"><img src="image/protegee.png" alt="indication géographique protégée" height="121" width="90" responsive-image></li>
+             </ul>
+            </div> 
+    </div>
+
+
+<footer>
+    
+    
+    
+    <section id="listnav" class="row">
+            <div class="offset-3 col-9">
+            <nav id="menu" class="col-12"> 
+                <ul class="list-inline" >
+                    <li><a href="clientLogin_1.php"/><button class="btn btn-primary " name="bouton" type="submit" alt="login">Déjà inscrit</button></a></li>
+                    <li class="space"> | </li>
+                    <li><a href="index.php"/>ACCUEIL</a></li>
+                    <li class="space"> | </li>
+                    <li><a href="reglement.php"/>REGLEMENT</a></li>
+                    <li class="space"> | </li>
+                    <li><a href="contact.php"/>CONTACT</a></li>
+                    <li class="space"> | </li>
+                    <li><a href="loginAdmin.php"/>ADMIN</a></li>
+                </ul>
+            </nav><!-- menu -->
+            </div>
+    </section>
+</footer>
